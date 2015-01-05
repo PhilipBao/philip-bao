d97f1a0f1aa6eb81c62bdc4cbdb12236a3429a14
bwwyyy <bwwyyy@gmail.com>
2015-01-04 19:11:39 -0500

    add_fea1

--- statics/js/main.js
+++ statics/js/main.js
@@ -24,6 +24,27 @@ var main = function() {
   });
   
   $('.btn').addClass('disabled');
+
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
 }
 
 $(document).ready(main);
